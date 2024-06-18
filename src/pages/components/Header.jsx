62f945914aaa8930b8f43c2bfba60996ce1b475b
renaxcardoso <81385406+RenanSilvaCardoso@users.noreply.github.com>
2024-06-17 21:00:57 -0300

    Desenvolvido tela de contato

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -7,13 +7,14 @@ function Header() {
     function Menu(e){
         let list = document.querySelector('ul');
 
+        console.log(e.target.getAttribute('name'));
         e.target.getAttribute('name') === 'menu' ? 
-        (e.target.setAttribute('name', 'close'),list.classList.add('right-[0]') , list.classList.add('opacity-100')) 
-        : ( e.target.setAttribute('name', 'menu'), list.classList.remove('right-[0]'),list.classList.remove('opacity-100'));
+        (e.target.setAttribute('name', 'close'), list.classList.remove('opacity-0') , list.classList.add('opacity-100'), list.classList.remove('left-[100%]'), list.classList.add('left-[0]')) 
+        : ( e.target.setAttribute('name', 'menu'), list.classList.remove('opacity-100'),list.classList.add('opacity-0'), list.classList.remove('left-[0]'), list.classList.add('left-[100%]'));
     }
 
     return (
-    <header class="w-full h-12 flex items-center justify-around">
+    <header class="w-full h-12 flex items-center justify-around relative">
         <nav class="px-5 py-2 bg-white shadow md:flex md:items-center md:justify-around w-full">
             <div class="flex justify-between items-center">
                 <span class="text-2xl cursor-pointer">
@@ -25,7 +26,7 @@ function Header() {
                 </span>
             </div>
 
-            <ul class="md:flex md:items-center md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 right-[200%] transition-all ease-in duration-300 text-center">
+            <ul class="md:flex md:items-center md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-100 left-[100%] transition-all ease-in duration-300 text-center">
                 <li class="mx-2 my-6 md:my-0">
                     <Link to="/" class="text-xl hover:text-primary duration-500">Início</Link>
                 </li>
