0ac8d47cec7cbc2a86bde02226efba87ba737f82
Otto Micheletti <michelettiotto@gmail.com>
2024-06-22 18:47:30 -0300

    implementa o iframe do Google Maps na tela de localização

--- client/src/pages/Location.jsx
+++ client/src/pages/Location.jsx
@@ -1,26 +1,13 @@
-import um from '../assets/img/crops1.jpg';
-import dois from '../assets/img/crops2.jpg';
-import tres from '../assets/img/crops3.jpg';
-import quatro from '../assets/img/crops4.jpg';
-
 export default function Location() {
     return (
-        <div className="w-full">
-            <div className="w-11/12 m-auto py-4 space-y-2">
+        <div className="w-full h-full flex flex-col items-center justify-items-start">
+            <div className="w-11/12 py-4 space-y-2">
                 <h1 className="text-2xl font-bold text-center mb-2">Venha nos conhecer!</h1>
-                <div className="w-full md:w-10/12 flex flex-wrap gap-1 m-auto">
-                    <img src={um} className="w-2/5 m-auto md:w-1/3 hover:scale-125 duration-300 shadow-md"/>
-                    <img src={dois} className="w-2/5 m-auto md:w-1/3 hover:scale-125 duration-300 shadow-md"/>
-                    <img src={tres} className="w-2/5 m-auto md:w-1/3 hover:scale-125 duration-300 shadow-md"/>
-                    <img src={quatro} className="w-2/5 m-auto md:w-1/3 hover:scale-125 duration-300 shadow-md"/>
-                </div>
-                <div className="w-full">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14306.95315529407!2d-48.8596267!3d-26.3025829!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb048633e9b39%3A0x6e90bd738bb1a635!2sCROPS%20%26%20Co%20Farmhouse!5e0!3m2!1spt-BR!2sbr!4v1718545519808!5m2!1spt-BR!2sbr"
-                        allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
-                        className="w-full h-44 md:w-3/5 m-auto"></iframe>
-                </div>
             </div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.738289532715!2d-48.86220162394057!3d-26.302582877016107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb048633e9b39%3A0x6e90bd738bb1a635!2sCROPS%20%26%20Co%20Farmhouse!5e0!3m2!1spt-BR!2sbr!4v1719091726882!5m2!1spt-BR!2sbr"
+                width="500" height="500" style={{border:0}} allowFullScreen="" loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
     )
 }
