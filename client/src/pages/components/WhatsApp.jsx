cf92f12bc599166aabbeb516e781e804aca19980
Otto Micheletti <michelettiotto@gmail.com>
2024-06-22 15:37:31 -0300

    ajustes no componente WhatsApp

--- client/src/pages/components/WhatsApp.jsx
+++ client/src/pages/components/WhatsApp.jsx
@@ -4,9 +4,9 @@ import {Link} from "react-router-dom";
 export default function WhatsApp() {
     return (
         <Link
-            className="absolute bottom-48 right-12 bg-green-500 w-[80px] h-[80px] flex justify-center items-center rounded-full shadow animate-bounce"
+            className="absolute bottom-28 right-6 bg-green-500 w-[60px] h-[60px] flex justify-center items-center rounded-full shadow animate-bounce"
             to="https://wa.me/5547988302999" target="_blank">
-            <FaWhatsapp size={60} color="white"/>
+            <FaWhatsapp size={40} color="white"/>
         </Link>
     )
 }
